e871744045cc18371fb01f3760663fc65ebae24f
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-12 16:42:38 +0530

    Auto push

--- ec2.tf
+++ ec2.tf
@@ -8,22 +8,22 @@ resource "aws_spot_instance_request" "spot" {
   vpc_security_group_ids   = [aws_security_group.allows_app.id] 
   subnet_id                = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
   tags = {
-    Name =  "${var.COMPONENT}=${var.ENV}"
+    Name =  "${var.COMPONENT}-${var.ENV}"
   }
 }
 
 # Creats ON DEMAND instances
 
 
-resource "aws_instance" "OD" {
+resource "aws_instance" "od" {
   count                     = var.OD_INSTANCE_COUNT
   ami                       = data.aws_ami.ami.image_id
   instance_type             = var.OD_INSTANCE_TYPE
   vpc_security_group_ids   = [aws_security_group.allows_app.id]
   subnet_id                = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
-  
+
  tags = {
-    Name = "${var.COMPONENT}=${var.ENV}"
+    Name = "${var.COMPONENT}-${var.ENV}"
   }
 }
 
