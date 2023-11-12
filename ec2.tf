099909c5a8fbe66b87e7f98ee78b030b6e04c0b9
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-12 16:17:18 +0530

    Auto push

--- ec2.tf
+++ ec2.tf
@@ -6,8 +6,7 @@ resource "aws_spot_instance_request" "spot" {
   instance_type            = var.SPOT_INSTANCE_TYPE
   wait_for_fulfillment     = true
   vpc_security_group_ids   = [aws_security_group.allows_app.id] 
-  subnet_id                = element(data.terraform_remote_state.vpc.output.PRIVATE_SUBNET_IDS, count.index)
-
+  subnet_id                = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
   tags = {
     Name =  "${var.COMPONENT}=${var.ENV}"
   }
@@ -21,10 +20,8 @@ resource "aws_instance" "OD" {
   ami                       = data.aws_ami.ami.image_id
   instance_type             = var.OD_INSTANCE_TYPE
   vpc_security_group_ids   = [aws_security_group.allows_app.id]
-  subnet_id                = element(data.terraform_remote_state.vpc.output.PRIVATE_SUBNET_IDS, count.index)
- 
-
-
+  subnet_id                = element(data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS, count.index)
+  
  tags = {
     Name = "${var.COMPONENT}=${var.ENV}"
   }
