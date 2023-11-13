5c79967c1604bc912f6b86d311861a087f189966
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-13 13:11:59 +0530

    Auto push

---
+++ sg.tf
@@ -0,0 +1,34 @@
+# Creates Security Group
+resource "aws_security_group" "allows_app" {
+  name         = "roboshop-${var.COMPONENT}-${var.ENV}"
+  description  = "roboshop-${var.COMPONENT}-${var.ENV}"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+  ingress {
+    description      = "Allow app only traffic"
+    from_port        = var.APP_PORT
+    to_port          = var.APP_PORT
+    protocol         = "tcp"
+    cidr_blocks      = [data.terraform_remote_state.vpc.outputs.VPC_CIDR, data.terraform_remote_state.vpc.outputs.DEFAULT_VPC_CIDR]
+  }
+
+
+egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "roboshop-${var.COMPONENT}-${var.ENV}"
+  }
+}
