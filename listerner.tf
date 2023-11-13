11936965899d9672ffef76beab3f0f9e442cca2d
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-14 00:22:43 +0530

    Auto push

--- listerner.tf
+++ listerner.tf
@@ -1,6 +1,6 @@
 # This creates the listener and adds to the priavte ALB
 
-resource "aws_lb_listener" "public" {
+resource "aws_lb_listener" "private" {
   count             = var.INTERNAL ? 1:0
 
   load_balancer_arn =  data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN
@@ -17,20 +17,20 @@ resource "aws_lb_listener" "public" {
 
 
 # Creats a rule in the private LoadBalancer Listtner
-resource "aws_lb_listener_rule" "app_rule" {
-  count               = var.INTERNAL ? 1 : 0
+# resource "aws_lb_listener_rule" "app_rule" {
+#   count               = var.INTERNAL ? 1 : 0
 
-  listener_arn        = aws_lb_listener.private.*.arn[0]
-  priority            = 99
+#   listener_arn        = aws_lb_listener.private.*.arn[0]
+#   priority            = 99
 
-  action {
-    type              = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
+#   action {
+#     type              = "forward"
+#     target_group_arn = aws_lb_target_group.app.arn
+#   }
 
-  condition {
-    host_header {
-      values = ["${var.COMPONENT}-${var.ENV}.data.terraform_remote_state.vpc.PRIVATE_HOSTED_ZONE_NAME"]
-    }
-  }
-}
+#   condition {
+#     host_header {
+#       values = ["${var.COMPONENT}-${var.ENV}.data.terraform_remote_state.vpc.PRIVATE_HOSTED_ZONE_NAME"]
+#     }
+#   }
+# }
