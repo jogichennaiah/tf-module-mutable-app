a1236f6f31bab848f515afc27efea3a76bfdf195
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-14 23:35:13 +0530

    Auto push

--- listerner_rule.tf
+++ listerner_rule.tf
@@ -1,6 +1,6 @@
 # This creates the listener and adds to the priavte ALB
 
-resource "aws_lb_listener" "public" {
+resource "aws_lb_listener" "private" {
   count             = var.INTERNAL ? 1:0
 
   load_balancer_arn =  data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ARN
@@ -25,8 +25,8 @@ resource "random_integer" "priority" {
 resource "aws_lb_listener_rule" "app_rule" {
   count               = var.INTERNAL ? 1 : 0
 
-  listener_arn        = aws_lb_listener.public.*.arn[0]
-  priority            = random_integer.priority
+  listener_arn        = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
+  priority            = random_integer.priority.result
 
   action {
     type              = "forward"
