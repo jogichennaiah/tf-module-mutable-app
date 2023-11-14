460b43832f1fb640c40a1d4f7ca567005177bb8e
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-15 00:01:11 +0530

    Auto push

--- targetGroup.tf
+++ targetGroup.tf
@@ -5,6 +5,17 @@ resource "aws_lb_target_group" "app" {
   port     = 8080
   protocol = "HTTP"
   vpc_id   = data.terraform_remote_state.vpc.outputs.VPC_ID
+
+
+health_check {
+  enabled              = true
+  healthy_threshold     = 2
+  interval             = 5
+  timeout              = 4
+  path                 = "/health"
+  unhealthy_threshold  = 2
+
+  }
 }
 
 # Attaches the target group to the LoadBalancer
@@ -13,5 +24,5 @@ resource "aws_lb_target_group_attachment" "attach_instnces" {
   count            = local.INSTANCE_COUNT
   target_group_arn = aws_lb_target_group.app.arn
   target_id        = element(local.INSTANCE_IDS, count.index)
-  port             = 8080
+  port             = var.APP.PORT
 }
