7ed5acb5721f1d3ebde82851ee4e4457d6d2e455
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-14 22:54:43 +0530

    Auto push

--- listerner.tf
+++ listerner.tf
@@ -16,21 +16,21 @@ resource "aws_lb_listener" "private" {
 }
 
 
-# Creats a rule in the private LoadBalancer Listtner
-# resource "aws_lb_listener_rule" "app_rule" {
-#   count               = var.INTERNAL ? 1 : 0
+#Creats a rule in the private LoadBalancer Listtner
+resource "aws_lb_listener_rule" "app_rule" {
+  count               = var.INTERNAL ? 1 : 0
 
-#   listener_arn        = aws_lb_listener.private.*.arn[0]
-#   priority            = 99
+  listener_arn        = aws_lb_listener.private.*.arn[0]
+  priority            = 99
 
-#   action {
-#     type              = "forward"
-#     target_group_arn = aws_lb_target_group.app.arn
-#   }
+  action {
+    type              = "forward"
+    target_group_arn = aws_lb_target_group.app.arn
+  }
 
-#   condition {
-#     host_header {
-#       values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"]
-#     }
-#   }
-# }
+  condition {
+    host_header {
+      values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"]
+    }
+  }
+}
