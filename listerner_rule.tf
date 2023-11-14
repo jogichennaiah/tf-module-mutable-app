9f7c9954f89dada495c94e1a3977114482f2b5e1
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-15 00:44:24 +0530

    Auto push

--- listerner_rule.tf
+++ listerner_rule.tf
@@ -1,17 +1,4 @@
-# Creates Public Listener
-# This creates the listener and adds to the private ALB
-resource "aws_lb_listener" "public" {
-  count             = var.INTERNAL ? 0 : 1
 
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-      type             = "forward"
-      target_group_arn = aws_lb_target_group.app.arn  
-  }
-}
 
 
 # Generates a Random Number in the given range 
