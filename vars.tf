087b2fb40345061ede3959ee0dd90ea8d3296750
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-17 18:11:16 +0530

    Auto push

--- vars.tf
+++ vars.tf
@@ -5,5 +5,6 @@ variable "SPOT_INSTANCE_TYPE" {}
 variable "SPOT_INSTANCE_COUNT" {}
 variable "OD_INSTANCE_TYPE" {}
 variable "OD_INSTANCE_COUNT" {}
+
 variable "APP_PORT" {}
 variable "APP_VERSION" {}
