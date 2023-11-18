bd289977b6e67581f7f40eccb40b03d9ef5bfd18
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-18 14:37:22 +0530

    Auto push

--- route53.tf
+++ route53.tf
@@ -1,6 +1,6 @@
-# # Create App Component Record against the ALB 
+ # Create App Component Record against the ALB 
 resource "aws_route53_record" "docdb_dns" {
-  zone_id = var.INTERNAL ? data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID
+  zone_id = var.INTERNAL ? [data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID : data.terraform_remote_state.vpc.outputs.PUBLIC_HOSTED_ZONE_ID]
   name    = "${var.COMPONENT}-${var.ENV}"
   type    = "CNAME"
   ttl     = 10
