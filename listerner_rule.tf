903a99e70485e73990b05e0acaab2dd41142b280
jogichennaiah <chennaiahjogi394@gmail.com>
2023-11-20 23:50:01 +0530

    Auto push

--- listerner_rule.tf
+++ listerner_rule.tf
@@ -4,7 +4,7 @@
 resource "aws_lb_listener" "public" {
   count             = var.INTERNAL ? 0 : 1
 
-  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_LISTENER_ARN
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
   port              = "80"
   protocol          = "HTTP"
 
